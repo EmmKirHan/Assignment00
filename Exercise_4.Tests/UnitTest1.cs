2d48e70a51799a1965a3d9175b84cfda14464f0d
Emma Kirstine Hansen <ekha@itu.dk>
2021-09-08 15:31:20 +0200

    Tested and implemented IsLeapYear

--- Exercise_4.Tests/UnitTest1.cs
+++ Exercise_4.Tests/UnitTest1.cs
@@ -3,12 +3,55 @@ using Xunit;
 
 namespace Exercise_4.Tests
 {
-    public class UnitTest1
+    public class LeapYearTest
     {
         [Fact]
-        public void Test1()
+        public void IsLeapYear_returns_true_for_input_divisible_by_four()
         {
+            Program program = new Program();
 
+            bool returned = program.IsLeapYear(8);
+
+            Assert.True(returned);
+        }
+
+        [Fact]
+        public void IsLeapYear_returns_false_for_input_not_divisible_by_four()
+        {
+            Program program = new Program();
+
+            bool returned = program.IsLeapYear(7);
+
+            Assert.False(returned);
+        }
+
+        [Fact]
+        public void IsLeapYear_returns_false_for_input_divisible_by_100()
+        {
+            Program program = new Program();
+
+            bool returned = program.IsLeapYear(100);
+
+            Assert.False(returned);
+        }
+
+        [Fact]
+        public void IsLeapYear_returns_true_for_input_divisible_by_4_not_100()
+        {
+            Program program = new Program();
+
+            bool returned = program.IsLeapYear(104);
+            Assert.True(returned);
+        }
+
+        [Fact]
+        public void IsLeapYear_returns_true_for_input_divisible_by_400()
+        {
+            Program program = new Program();
+
+            bool returned = program.IsLeapYear(400);
+
+            Assert.True(returned);
         }
     }
 }
