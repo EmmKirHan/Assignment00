f14e38093eff600a16659777df8810952ebd15ef
Emma Kirstine Hansen <ekha@itu.dk>
2021-09-09 13:13:28 +0200

    Implemented error handling

--- Exercise_4.Tests/UnitTest1.cs
+++ Exercise_4.Tests/UnitTest1.cs
@@ -75,5 +75,35 @@ namespace Exercise_4.Tests
             var output = writer.GetStringBuilder().ToString().Trim();
             Assert.Equal("nay", output);
         }
+
+        [Fact]
+        public void CheckInput_prints_error_if_year_is_too_low()
+        {
+            var writer = new StringWriter();
+            Console.SetOut(writer);
+
+            var userInput = new StringReader("1400");
+            Console.SetIn(userInput);
+
+            Program.Main(new string[0]);
+
+            var output = writer.GetStringBuilder().ToString().Trim();
+            Assert.Contains("Smallest allowed value is 1582. Try again, or press Space to quit", output);
+        }
+
+        [Fact]
+        public void CheckInput_prints_error_if_input_is_not_integer()
+        {
+            var writer = new StringWriter();
+            Console.SetOut(writer);
+
+            var userInput = new StringReader("hej med dig");
+            Console.SetIn(userInput);
+
+            Program.Main(new string[0]);
+
+            var output = writer.GetStringBuilder().ToString().Trim();
+            Assert.Contains("Please enter an integer value or press Space to quit", output);
+        }
     }
 }
--- Exercise_4/Program.cs
+++ Exercise_4/Program.cs
@@ -6,16 +6,44 @@ namespace Exercise_4
     {
         public static void Main(string[] args)
         {
-            string input = Console.ReadLine();
-            int year = Int32.Parse(input);
-            bool isLeapYear = IsLeapYear(year);
-            if (isLeapYear)
+            string input = ReadInput();
+            CheckInput(input);
+        }
+
+        public static void CheckInput(string input)
+        {
+
+            if (input == "Empty")
             {
-                Console.Write("yay");
+                Console.WriteLine("Goodbye");
             }
             else
             {
-                Console.Write("nay");
+                if (Int32.TryParse(input, out int year))
+                {
+                    if (year < 1582)
+                    {
+                        Console.WriteLine("Smallest allowed value is 1582. Try again, or press Space to quit");
+                        CheckInput(ReadInput());
+                    }
+                    else
+                    {
+                        bool isLeapYear = IsLeapYear(year);
+                        if (isLeapYear)
+                        {
+                            Console.Write("yay");
+                        }
+                        else
+                        {
+                            Console.Write("nay");
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Please enter an integer value or press Space to quit");
+                    CheckInput(ReadInput());
+                }
             }
         }
 
@@ -37,6 +65,21 @@ namespace Exercise_4
             {
                 return false;
             }
+        }
+
+        private static string ReadInput()
+        {
+            string input = "Empty";
+            string lineRead = Console.ReadLine();
+            if (!String.IsNullOrEmpty(lineRead))
+            {
+                input = lineRead;
+            }
+            else
+            {
+                return input;
+            }
+            return input;
         }
 
     }
