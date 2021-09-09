eb7856f46f6be5fdaf448775bd2e5b080fa75964
EmmKirHan <75368461+EmmKirHan@users.noreply.github.com>
2021-09-09 16:28:34 +0200

    Feature/leap year (#2)
    * Adds console application and xunit test
    
    * Tested and implemented IsLeapYear
    
    * Created user interface for IsLeapYear
    
    * Implemented error handling
    
    * Added documentation
    
    Co-authored-by: Emma Kirstine Hansen <ekha@itu.dk>

--- Exercise_4.Tests/UnitTest1.cs
+++ Exercise_4.Tests/UnitTest1.cs
@@ -1,14 +1,109 @@
 using System;
+using System.IO;
 using Xunit;
 
 namespace Exercise_4.Tests
 {
-    public class UnitTest1
+    public class LeapYearTest
     {
         [Fact]
-        public void Test1()
+        public void IsLeapYear_returns_true_for_input_divisible_by_four()
         {
+            bool returned = Program.IsLeapYear(8);
 
+            Assert.True(returned);
+        }
+
+        [Fact]
+        public void IsLeapYear_returns_false_for_input_not_divisible_by_four()
+        {
+            bool returned = Program.IsLeapYear(7);
+
+            Assert.False(returned);
+        }
+
+        [Fact]
+        public void IsLeapYear_returns_false_for_input_divisible_by_100()
+        {
+            bool returned = Program.IsLeapYear(100);
+
+            Assert.False(returned);
+        }
+
+        [Fact]
+        public void IsLeapYear_returns_true_for_input_divisible_by_4_not_100()
+        {
+            bool returned = Program.IsLeapYear(104);
+
+            Assert.True(returned);
+        }
+
+        [Fact]
+        public void IsLeapYear_returns_true_for_input_divisible_by_400()
+        {
+            bool returned = Program.IsLeapYear(400);
+
+            Assert.True(returned);
+        }
+
+        [Fact]
+        public void Main_prints_yay_if_input_is_leap_year()
+        {
+            var writer = new StringWriter();
+            Console.SetOut(writer);
+
+            var userInput = new StringReader("2000");
+            Console.SetIn(userInput);
+
+            Program.Main(new string[0]);
+
+            var output = writer.GetStringBuilder().ToString().Trim();
+            Assert.Equal("yay", output);
+        }
+
+        [Fact]
+        public void Main_prints_nay_if_input_is_not_leap_year()
+        {
+            var writer = new StringWriter();
+            Console.SetOut(writer);
+
+            var userInput = new StringReader("1997");
+            Console.SetIn(userInput);
+
+            Program.Main(new string[0]);
+
+            var output = writer.GetStringBuilder().ToString().Trim();
+            Assert.Equal("nay", output);
+        }
+
+        [Fact]
+        public void CheckInput_prints_error_if_year_is_too_low()
+        {
+            var writer = new StringWriter();
+            Console.SetOut(writer);
+
+            var userInput = new StringReader("1400");
+            Console.SetIn(userInput);
+
+            Program.Main(new string[0]);
+
+            var output = writer.GetStringBuilder().ToString().Trim();
+            Assert.Contains("Smallest allowed value is 1582. Try again, or press Space to quit", output);
+        }
+
+        [Fact]
+        public void CheckInput_prints_error_if_input_is_not_integer()
+        {
+            var writer = new StringWriter();
+            Console.SetOut(writer);
+
+            var userInput = new StringReader("hej med dig");
+            Console.SetIn(userInput);
+
+            Program.Main(new string[0]);
+
+            var output = writer.GetStringBuilder().ToString().Trim();
+            Assert.Contains("Please enter an integer value or press Space to quit", output);
         }
     }
 }
--- Exercise_4/Program.cs
+++ Exercise_4/Program.cs
@@ -2,11 +2,85 @@
 
 namespace Exercise_4
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string input = ReadInput();
+            CheckInput(input);
         }
+
+        public static void CheckInput(string input)
+        {
+
+            if (input == "Empty")
+            {
+                Console.WriteLine("Goodbye");
+            }
+            else
+            {
+                if (Int32.TryParse(input, out int year))
+                {
+                    if (year < 1582)
+                    {
+                        Console.WriteLine("Smallest allowed value is 1582. Try again, or press Space to quit");
+                        CheckInput(ReadInput());
+                    }
+                    else
+                    {
+                        bool isLeapYear = IsLeapYear(year);
+                        if (isLeapYear)
+                        {
+                            Console.Write("yay");
+                        }
+                        else
+                        {
+                            Console.Write("nay");
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Please enter an integer value or press Space to quit");
+                    CheckInput(ReadInput());
+                }
+            }
+        }
+
+        public static bool IsLeapYear(int year)
+        {
+            if (year % 4 == 0)
+            {
+                if (year % 100 == 0 && year % 400 != 0)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private static string ReadInput()
+        {
+            string input = "Empty";
+            string lineRead = Console.ReadLine();
+            if (!String.IsNullOrEmpty(lineRead))
+            {
+                input = lineRead;
+            }
+            else
+            {
+                return input;
+            }
+            return input;
+        }
+
     }
 }
